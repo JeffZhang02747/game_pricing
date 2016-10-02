d18b16a306a1af9869d24d67a4dc687f612fde00
Jeff Zhang <a920249925@gmail.com>
2016-10-01 22:15:27 -0400

    working on the functionality of searching games

---
+++ meta.py
@@ -0,0 +1,28 @@
+#img url example :
+#https://store.playstation.com/store/api/chihiro/00_09_000/container/CA/en/999/UP9000-CUSA00900_00-SPEXPANSIONDLC03/1474345129000/image?_version=00_09_000&platform=chihiro&w=248&h=248&bg_color=000000&opacity=100
+
+#search url example :
+#https://store.playstation.com/chihiro-api/bucket-search/US/en/999/bloodborne?size=30&geoCountry=CA
+
+
+#https://store.playstation.com/chihiro-api/search/CA/en/999/god%2Bof%2Bwar?bucket=games&game_content_type=games&size=30&geoCountry=CA
+import requests
+import urllib
+
+
+
+
+search_url = "https://store.playstation.com/chihiro-api/search/CA/en/999/%s?bucket=games&game_content_type=games&size=30&geoCountry=CA"
+'''
+search games on ps store
+'''
+def search_games_ps_store(query):
+	#first urllib encode the query so it can fit into the url
+	query = urllib.quote(query)
+	to_search_url = search_url % (query)
+
+	#simple get request
+	req = requests.get(to_search_url)
+	response = req.json()
+
+	
